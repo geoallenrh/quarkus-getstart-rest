41873bee1fbbdaee028cf0256fea5c21d98caeeb
geoallenrh <geoffallen@redhat.com>
2020-11-18 09:04:38 -0600

    nice day

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hola everyone";
+        return "Have a Nice Day";
     }
 }
