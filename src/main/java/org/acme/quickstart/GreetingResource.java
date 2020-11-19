c2410ae1b37fd530d80b578cc83e9014c355c61a
geoallenrh <geoffallen@redhat.com>
2020-11-18 18:57:28 -0600

    wonderful

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Have a Great Day";
+        return "Have a Wonderful Day";
     }
 }
